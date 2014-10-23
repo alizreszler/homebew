7cf6bb02afb6002d36cc4872b0322f4fe1e4a900
alizreszler <alizreszler@alizs-mbp.att.net>
2014-10-22 22:12:17 -0700

    Added initial project

---
+++ MusicSplit/MusicSplit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicSplit
+//
+//  Created by Aliz Reszler on 10/22/14.
+//  Copyright (c) 2014 HomeBrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
