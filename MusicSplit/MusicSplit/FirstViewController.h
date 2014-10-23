8c92877e82d0b589fe3b98a468783f3bdc4c7324
Aliz Reszler <aliz.reszler@gmail.com>
2014-10-22 23:26:24 -0700

    Added back project

---
+++ MusicSplit/MusicSplit/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  MusicSplit
+//
+//  Created by Aliz Reszler on 10/22/14.
+//  Copyright (c) 2014 HomeBrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
